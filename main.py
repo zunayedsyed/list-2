279fe4170efd07b50ac412f1ddd0009718a35827
zunayedsyed <71093884+zunayedsyed@users.noreply.github.com>
2021-06-10 18:54:05 +0600

    list
    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# list
+# create a function
+def list():
+	x=[5,6,7,8]
+	y=len(x)
+	sum=2
+	sum*=y
+	print("sum=",sum)
+# call function
+list()   
+# list and conditional logic
+# create a function
+def list():
+	x=[5,6,7,8]
+	y=int(input())
+	sum=5
+	if y in x:
+		sum+=y
+		print(sum)
+	else:
+		print("error")
+# call a function 
+list()
+# list and Loop
+# create a function 
+def list():
+	x=[1,2,3]
+	for b in x:
+		print(b)
+# function call 
+list()
